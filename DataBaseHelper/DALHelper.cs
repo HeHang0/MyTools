41109878c022cadb9b687617960d446069e0680a
HeHang <hehang724590957@qq.com>
2018-08-11 19:21:02 +0800

    添加 - 【修改数据库条目】

--- DataBaseHelper/DALHelper.cs
+++ DataBaseHelper/DALHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -18,26 +19,48 @@ namespace DataBaseHelper
         /// <param name="model">要更新的Model，可以为匿名对象</param>
         /// <param name="condition">推荐使用参数式写法</param>
         /// <returns></returns>
-        public static int Modify(string tableName, object model, string condition = "")
+        public static int Modify(string connectionString, string tableName, object model, string condition = "")
         {
-            Dictionary<string, string> paramsDic = GetModelDic(model);
+            Dictionary<string, object> paramsDic = GetModelDic(model);
             string paramsSql = string.Join(", ", paramsDic.Select(m => $"{m.Key}=@{m.Key}"));
             string sql = $"UPDATE {tableName} SET {paramsSql} {(string.IsNullOrWhiteSpace(condition) ? "" : " WHERE ")} {condition}";
+            SqlParameter[] sqlParameters = GetSqlParameters(paramsDic);
+            return new DBHelper(connectionString).ExecuteCommand(sql, sqlParameters);
         }
 
         #endregion
 
 
-        #region 【获取model对象的属性值】
+        #region 【对传入的Model对象进行处理】
 
-        public static Dictionary<string, string> GetModelDic(object model)
+        /// <summary>
+        /// 获取model对象的属性值
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        private static Dictionary<string, object> GetModelDic(object model)
         {
-            Dictionary<string, string> dic = new Dictionary<string, string>();
+            Dictionary<string, object> dic = new Dictionary<string, object>();
             foreach (PropertyInfo p in model.GetType().GetProperties())
             {
-                dic[p.Name] = p.GetValue(model).ToString();
+                dic[p.Name] = p.GetValue(model);
             }
             return dic;
+        }
+
+        private static SqlParameter[] GetSqlParameters(Dictionary<string, object> modelDic)
+        {
+            if (modelDic == null)
+            {
+                return new SqlParameter[0];
+            }
+            SqlParameter[] sqlParameters = new SqlParameter[modelDic.Count];
+            int index = 0;
+            foreach (var item in modelDic)
+            {
+                sqlParameters[index++] = new SqlParameter($"@{item.Key}", item.Value);
+            }
+            return sqlParameters;
         }
 
         #endregion
