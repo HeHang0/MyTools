64ee41cf6bdffe317bda8c76dae862426eff3088
何航 <hehang724590957@qq.com>
2018-08-14 00:11:27 +0800

    添加 - 【SignalR Console客户端】

---
+++ SignalRClient/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SignalRClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入用户名：");
+            var name = Console.ReadLine();
+            SignalRClient src = new SignalRClient("https://localhost:44368/chatHub", name);
+            src.OnReceiveMessage += OnReceiveMessage;
+            Console.WriteLine("输入\\q退出");
+            string messgae = "";
+            while (true)
+            {
+                //Console.Write($"{name} : ");
+                messgae = Console.ReadLine();
+                if (messgae == "\\q")
+                {
+                    break;
+                }
+                Task<(bool isSend, string info)> task =  src.SendMessageAsync(messgae);
+                task.ContinueWith(r =>
+                {
+                    if (!r.Result.isSend)
+                    {
+                        Console.WriteLine(r.Result.info);
+                    }
+                });
+            }
+        }
+
+        private static void OnReceiveMessage(string name, string message)
+        {
+            Console.WriteLine($"{name} : {message}");
+        }
+    }
+}
---
+++ SignalRClient/SignalRClient.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalRClient
+{
+    public class SignalRClient
+    {
+        HubConnection connection;
+        public delegate void ReceiveMessageHandler(string name, string message);
+        public event ReceiveMessageHandler OnReceiveMessage;
+        private string name;
+        public SignalRClient(string url, string name)
+        {
+            this.name = name;
+            connection = new HubConnectionBuilder()
+            .WithUrl(url)
+            .Build();
+            Connect();
+            connection.StartAsync();
+        }
+
+        private void Connect()
+        {
+            connection.On<string, string>("ReceiveMessage", (user, message) =>
+            {
+                OnReceiveMessage?.Invoke(user, message);
+            });
+        }
+
+        public async Task<(bool isSend, string info)> SendMessageAsync(string message)
+        {
+            var (isSend, info) = (false, "");
+            try
+            {
+                await connection.InvokeAsync("SendMessage",
+                        name, message);
+                isSend = true; info = "Success";
+            }
+            catch (Exception ex)
+            {
+                isSend = false;
+                info = ex.Message;
+            }
+            return (isSend, info);
+        }
+    }
+}
